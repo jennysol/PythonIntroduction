15d1e31f0de800666818a4a5077413c2ab25e584
jennysol <jennyssoliver@gmail.com>
2020-09-21 21:08:29 -0300

    Iniciando conceitos de listas compostas

--- Cursopython/aula018.py
+++ Cursopython/aula018.py
@@ -1,1 +1,14 @@
-# Listas 
+# Listas Compostas
+
+# pessoas.append(dados[:]) Buscar uma estrutura, copiar e juntar
+# pessoas = [['Pedro', 25], ['Maria',19]] > Listas compostas
+
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(galera)
