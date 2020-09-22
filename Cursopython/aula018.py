6f01b3b3c4f00f4f66b2c064eb5a2ce4f089995e
jennysol <jennyssoliver@gmail.com>
2020-09-22 20:46:40 -0300

    Adicionando exemplo de lista composta

--- Cursopython/aula018.py
+++ Cursopython/aula018.py
@@ -3,12 +3,30 @@
 # pessoas.append(dados[:]) Buscar uma estrutura, copiar e juntar
 # pessoas = [['Pedro', 25], ['Maria',19]] > Listas compostas
 
-teste = list()
-teste.append('Gustavo')
-teste.append(40)
+# teste = list()
+# teste.append('Gustavo')
+# teste.append(40)
+# galera = list()
+# galera.append(teste[:])
+# teste[0] = 'Maria'
+# teste[1] = 22
+# galera.append(teste[:])
+# print(galera)
+
 galera = list()
-galera.append(teste[:])
-teste[0] = 'Maria'
-teste[1] = 22
-galera.append(teste[:])
-print(galera)
+dado = list() # Estrutura auxiliar para transmitir dados
+totmai = totmen = 0
+for c in range(0,3):
+    dado.append(str(input('Nome:')))
+    dado.append(int(input('Idade:')))
+    galera.append(dado[:])
+    dado.clear() # parametro para limpar todos os itens da lista.
+
+for p in galera:
+    if p[1] >= 18:
+        print(f'{p[0]} é maior de idade.')
+        totmai += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmen += 1
+print(f'Temos {totmai} maiores e {totmen} menores de idade')
