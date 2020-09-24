ba768b65a92c66fa3a17dfc8ed50836bd028d00c
jennysol <jennyssoliver@gmail.com>
2020-09-24 17:31:35 -0300

    Adicionando ex86 matriz

---
+++ Cursopython/PythonExercicios/ex086.py
@@ -0,0 +1,13 @@
+# Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. 
+# No final, mostre a matriz na tela, com a formatação correta.
+
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor [{l}, {c}]: '))
+
+print('-´-'*20)
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz [l][c]:^5}]', end='')
+    print()# Método para quebrar linha
