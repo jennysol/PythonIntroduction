b1425f4781f3c2a54aaedbe8a7fda83d651dea3f
jennysol <jennyssoliver@gmail.com>
2020-10-07 17:20:19 -0300

    Adicionando ex095 dict

--- Cursopython/PythonExercicios/ex095.py
+++ Cursopython/PythonExercicios/ex095.py
@@ -9,7 +9,7 @@ while True:
     partidas.clear()
 
     for c in range(0, tot):
-        partidas.append(int(input(f'Quantos gols na partida {c}: ')))
+        partidas.append(int(input(f'Quantos gols na partida {c+1}: ')))
 
     jogador['gols'] = partidas[:]
     jogador['total'] = sum(partidas)
@@ -23,10 +23,31 @@ while True:
     if resp == 'N':
         break
 
+# Mostrar os dados
+print('-=' *30)
+print('cod', end='')
+for i in jogador.keys():
+    print(f' {i:<15}', end='')
+print()
+
 print('-.-'*30)
 for k, v in enumerate(time):
-    print(f'{k:>5}', end='')
+    print(f'{k:>3}', end='')
     for d in v.values():
         print(f'{str(d):<15}', end='')
     print()
 print('-.-'*30)
+
+while True:
+    busca = int(input("Mostrar dados de qual jogador? (999 para parar) "))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com código {busca}!')
+    else:
+        print(f' --Levantamento do jogador {time[busca]["nome"]}:')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'  No jogo {i} fez {g} gols.')
+    print('-'*40)
+print('---Volte sempre.----')
+
