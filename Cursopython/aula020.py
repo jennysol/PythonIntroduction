63255f54967bc4ca55e8f88bb619f319f025e126
jennysol <jennyssoliver@gmail.com>
2020-10-10 17:33:24 -0300

    Finalizando conceitos de funçoes

--- Cursopython/aula020.py
+++ Cursopython/aula020.py
@@ -1,9 +1,32 @@
 # FUNÇÕES
+# Coisas que acontecem constantemente é sinal que precisa-se usar funções
 
-def titulo(txt):
-    print('-'*30)
-    print(txt)
-    print('-'*30)
+# def titulo(txt):
+#     print('-'*30)
+#     print(txt)
+#     print('-'*30)
 
-titulo('Jennifer')
-titulo('Linda')
+# titulo('Jennifer')
+# titulo('Linda')
+
+# a = 4
+# # b = 5
+# # s = a + b
+
+# # def soma(a,b):
+# #     s = a +b 
+# #     print(s)
+
+# # soma(4, 5)
+# # soma(8, 9)
+
+# soma(b=4, a=5)
+
+'''
+Empacotar parâmetros
+
+def contador(*núm):
+
+contador(5,7,3,1,4)
+contador( 8,4,7)
+'''
