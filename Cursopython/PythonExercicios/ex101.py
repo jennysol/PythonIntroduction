195bbffc392245a2bf4006cddcbb606b8fa6a1b0
jennysol <jennyssoliver@gmail.com>
2020-11-01 22:09:53 -0300

    Adicionando ex101 funçoes

---
+++ Cursopython/PythonExercicios/ex101.py
@@ -0,0 +1,21 @@
+# Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro 
+# o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem 
+# voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
+
+from datetime import date
+
+
+def voto(ano):
+    from datetime import date
+
+    atual = date.today().year
+    idade = atual - ano
+
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+print(voto(2000))
